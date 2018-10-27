bcbfaf07e5fcfc06472262168b5d01ec18e96391
Cristinel Mateias <c.mateias@>
2018-10-27 23:28:29 +0100

    giphy tidy

--- OfficeCulture.Data/Models/Attachment.cs
+++ OfficeCulture.Data/Models/Attachment.cs
@@ -36,7 +36,7 @@ namespace OfficeCulture.Data.Models
         public Field[] Fields { get; set; }
 
         [JsonProperty("image_url")]
-        public string ImageUrl { get; set; }
+        public Uri ImageUrl { get; set; }
 
         [JsonProperty("thumb_url")]
         public Uri ThumbUrl { get; set; }
