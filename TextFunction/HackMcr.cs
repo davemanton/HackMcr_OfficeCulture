df3310203bf5a41019bfb788cac5f4d7fa173f8d
Dave Manton <dave.manton@deptagency.com>
2018-10-27 17:12:15 +0100

    Slack stuff

--- TextFunction/HackMcr.cs
+++ TextFunction/HackMcr.cs
@@ -11,7 +11,7 @@ namespace TextFunction
     public static class HackMcr
     {
         [FunctionName("hackmcr")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
@@ -20,9 +20,25 @@ namespace TextFunction
                 .FirstOrDefault(q => string.Compare(q.Key, "content", true) == 0)
                 .Value;
 
-            log.Info(content);
+            var client = new HttpClient();
+
+            var slackMessage = new SlackMessage
+            {
+                text = content
+            };
+
+            client.PostAsJsonAsync("https://hooks.slack.com/services/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", slackMessage);
+
+            client.GetAsync("https://api.clockworksms.com/http/send.aspx?key=a15795bf55cf6acaf6061be7af26bbb86bc22c52&to=447950580480&content=Hello+World");
 
             return req.CreateResponse(HttpStatusCode.OK, content);
         }
+
+
+    }
+
+    public class SlackMessage
+    {
+        public string text { get; set; }
     }
 }
