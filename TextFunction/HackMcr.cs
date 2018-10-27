b71f15f08485d7f89e47e325bc9c82804c67a5f4
Dave Manton <dave.manton@deptagency.com>
2018-10-27 18:31:20 +0100

    Auto stash before merge of "master" and "HackMcr/master"

--- TextFunction/HackMcr.cs
+++ TextFunction/HackMcr.cs
@@ -2,9 +2,11 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using OfficeCulture.Luis;
 using OfficeCulture.Data.Models;
 using OfficeCulture.Sounds.Manager;
 
@@ -15,7 +17,7 @@ namespace TextFunction
         private static string _slackWebHook = "https://hooks.slack.com/services/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";
 
         [FunctionName("hackmcr")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
             
@@ -24,29 +26,45 @@ namespace TextFunction
                 .FirstOrDefault(q => string.Compare(q.Key, "content", true) == 0)
                 .Value;
 
-            SendTextMessage(content);
-            SendSoundTextMessage(content);
+            string from = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "from", true) == 0)
+                .Value;
 
-            //client.GetAsync("https://api.clockworksms.com/http/send.aspx?key=a15795bf55cf6acaf6061be7af26bbb86bc22c52&to=447950580480&content=Hello+World");
+            var luisClient = new LuisClient();
+            var luisData = await luisClient.AnalyseText(content);
 
+            // bold out entity pieces in original message
+            if(luisData.Entities != null)
+            {
+                var counter = 0;
+                foreach(var entity in luisData.Entities)
+                {
+                    content = content.Insert(entity.StartIndex + counter++, "*");
+                    content = content.Insert(entity.EndIndex + counter++ + 1, "*");
+                }
+            }
+
+            var searchKeywords = string.Join(" ", luisData.Entities.Select(x => x.Entity));
+
+            var client = new HttpClient();
+
+            SendTextMessage(client, content, searchKeywords);
+            SendSoundTextMessage(client, content);            
+            
             return req.CreateResponse(HttpStatusCode.OK, content);
         }
 
-        public static void SendTextMessage(string query)
+        
+        public static void SendTextMessage(HttpClient client, string from, string searchKeywords)
         {
-            var slackMessage = new SlackMessage
-            {
-                text = query
-            };
-            HttpClient client = new HttpClient();
-
-            client.PostAsJsonAsync(_slackWebHook, slackMessage);
+            if (!string.IsNullOrWhiteSpace(from) && !string.IsNullOrWhiteSpace(searchKeywords))
+                client.GetAsync("https://api.clockworksms.com/http/send.aspx?key=a15795bf55cf6acaf6061be7af26bbb86bc22c52&to={from}&content=You%27re giphin about {searchKeywords}");
         }
 
-        public static void SendSoundTextMessage(string query)
+        public static void SendSoundTextMessage(HttpClient client, string message)
         {
             var soundManager = new SoundManager();
-            var sound = soundManager.RunAsync(query).Result;
+            var sound = soundManager.RunAsync(message).Result;
             
             var soundSlackMessage = new SoundSlackMessage
             {
@@ -61,7 +79,7 @@ namespace TextFunction
                 //    }
                 //}
             };
-            HttpClient client = new HttpClient();
+
             client.PostAsJsonAsync(_slackWebHook, soundSlackMessage);
         }
     }
