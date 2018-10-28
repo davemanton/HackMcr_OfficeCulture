72e234ab187e59a23acf67d266edf4252bdbcd68
Dave Manton <dave.manton@deptagency.com>
2018-10-28 09:20:03 +0000

    Last db entries

--- TextFunction/HackMcr.cs
+++ TextFunction/HackMcr.cs
@@ -85,6 +85,7 @@ namespace TextFunction
                     Sound = $"https:{soundUrl}",
                     Gif = imageUrl,
                     Sentiment = luisData.SentimentAnalysis.Label,
+                    Translations = translations,
                     Timestamp = DateTime.Now
                 });
 
